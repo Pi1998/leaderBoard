b61b00c965c942483740f468fe33d9215f4b26b1
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-09-01 01:27:30 +0630

    Add array for score list data, eventListener for form submission

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import './style.css';
+import { renderList, addScore } from './modules/renderList';
+
+
+const scoreListData = [
+    {
+        id: 1,
+        name: 'Shinn',
+        score: 100,
+    },
+    {
+        id: 2,
+        name: 'Thant',
+        score: 100,
+    },
+    {
+        id: 3,
+        name: 'Swam',
+        score: 100,
+    },
+    {
+        id: 4,
+        name: 'Ye',
+        score: 100,
+    }
+];
+
+const scoreList = document.getElementById('score-list');
+
+const addScoreList = document.getElementById('add-score-list');
+addScoreList.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nameInput = document.getElementById('name-input').value;
+    const scoreInput = document.getElementById('score-input').value;
+    addScore({nameInput, scoreInput});
+
+});
+
+window.onload=()=>{
+    renderList(scoreListData, scoreList );
+};
